bc1035599a3de2b46559715223688aa367a78f26
Monty <monty@mariadb.org>
2018-02-14 14:41:06 +0200

    Changed static const in Alter_info and Alter_online_info to defines
    Main reason was to make it easier to print the above structures in
    a debugger. Additional benefits is that I was able to use same
    defines for both structures, which simplifes some code.
    
    Most of the code is just removing Alter_info:: and Alter_inplace_info::
    from alter table flags.
    
    Following renames was done:
    HA_ALTER_FLAGS        -> alter_table_operations
    CHANGE_CREATE_OPTION  -> ALTER_CHANGE_CREATE_OPTION
    Alter_info::ADD_INDEX -> ALTER_ADD_INDEX
    DROP_INDEX            -> ALTER_DROP_INDEX
    ADD_UNIQUE_INDEX      -> ALTER_ADD_UNIQUE_INDEX
    DROP_UNIQUE_INDEx     -> ALTER_DROP_UNIQUE_INDEX
    ADD_PK_INDEX          -> ALTER_ADD_PK_INDEX
    DROP_PK_INDEX         -> ALTER_DROP_PK_INDEX
    Alter_info:ALTER_ADD_COLUMN    -> ALTER_PARSE_ADD_COLUMN
    Alter_info:ALTER_DROP_COLUMN   -> ALTER_PARSE_DROP_COLUMN
    Alter_inplace_info::ADD_INDEX  -> ALTER_ADD_NON_UNIQUE_NON_PRIM_INDEX
    Alter_inplace_info::DROP_INDEX -> ALTER_DROP_NON_UNIQUE_NON_PRIM_INDEX
    
    Other things:
    - Added typedef alter_table_operatons for alter table flags
    - DROP CHECK CONSTRAINT can now be done online
    - Added checks for Aria tables in alter_table_online.test
    - alter_table_flags now takes an ulonglong as argument.
    - Don't support online operations if checksum option is used.
    - sql_lex.cc doesn't add ALTER_ADD_INDEX if index is not created

--- sql/sql_alter.h
+++ sql/sql_alter.h
@@ -29,77 +29,6 @@ class Key;
 class Alter_info
 {
 public:
-  /*
-    These flags are set by the parser and describes the type of
-    operation(s) specified by the ALTER TABLE statement.
-
-    They do *not* describe the type operation(s) to be executed
-    by the storage engine. For example, we don't yet know the
-    type of index to be added/dropped.
-  */
-
-  enum operations_used_flags
-  {
-    // Set for ADD [COLUMN]
-    ALTER_ADD_COLUMN            = 1L <<  0,
-    // Set for DROP [COLUMN]
-    ALTER_DROP_COLUMN           = 1L <<  1,
-    // Set for CHANGE [COLUMN] | MODIFY [CHANGE] & mysql_recreate_table
-    ALTER_CHANGE_COLUMN         = 1L <<  2,
-    // Set for ADD INDEX | ADD KEY | ADD PRIMARY KEY | ADD UNIQUE KEY |
-    //         ADD UNIQUE INDEX | ALTER ADD [COLUMN]
-    ALTER_ADD_INDEX             = 1L <<  3,
-    // Set for DROP PRIMARY KEY | DROP FOREIGN KEY | DROP KEY | DROP INDEX
-    ALTER_DROP_INDEX            = 1L <<  4,
-    // Set for RENAME [TO]
-    ALTER_RENAME                = 1L <<  5,
-    // Set for ORDER BY
-    ALTER_ORDER                 = 1L <<  6,
-    // Set for table_options
-    ALTER_OPTIONS               = 1L <<  7,
-    // Set for ALTER [COLUMN] ... SET DEFAULT ... | DROP DEFAULT
-    ALTER_CHANGE_COLUMN_DEFAULT = 1L <<  8,
-    // Set for DISABLE KEYS | ENABLE KEYS
-    ALTER_KEYS_ONOFF            = 1L <<  9,
-    // Set for FORCE, ENGINE(same engine), by mysql_recreate_table()
-    ALTER_RECREATE              = 1L << 10,
-    // Set for ADD PARTITION
-    ALTER_ADD_PARTITION         = 1L << 11,
-    // Set for DROP PARTITION
-    ALTER_DROP_PARTITION        = 1L << 12,
-    // Set for COALESCE PARTITION
-    ALTER_COALESCE_PARTITION    = 1L << 13,
-    // Set for REORGANIZE PARTITION ... INTO
-    ALTER_REORGANIZE_PARTITION  = 1L << 14,
-    // Set for partition_options
-    ALTER_PARTITION             = 1L << 15,
-    // Set for LOAD INDEX INTO CACHE ... PARTITION
-    // Set for CACHE INDEX ... PARTITION
-    ALTER_ADMIN_PARTITION       = 1L << 16,
-    // Set for REORGANIZE PARTITION
-    ALTER_TABLE_REORG           = 1L << 17,
-    // Set for REBUILD PARTITION
-    ALTER_REBUILD_PARTITION     = 1L << 18,
-    // Set for partitioning operations specifying ALL keyword
-    ALTER_ALL_PARTITION         = 1L << 19,
-    // Set for REMOVE PARTITIONING
-    ALTER_REMOVE_PARTITIONING   = 1L << 20,
-    // Set for ADD FOREIGN KEY
-    ADD_FOREIGN_KEY             = 1L << 21,
-    // Set for DROP FOREIGN KEY
-    DROP_FOREIGN_KEY            = 1L << 22,
-    // Set for EXCHANGE PARITION
-    ALTER_EXCHANGE_PARTITION    = 1L << 23,
-    // Set by Sql_cmd_alter_table_truncate_partition::execute()
-    ALTER_TRUNCATE_PARTITION    = 1L << 24,
-    // Set for ADD [COLUMN] FIRST | AFTER
-    ALTER_COLUMN_ORDER          = 1L << 25,
-    ALTER_ADD_CHECK_CONSTRAINT  = 1L << 27,
-    ALTER_DROP_CHECK_CONSTRAINT = 1L << 28,
-    ALTER_COLUMN_UNVERSIONED    = 1L << 29,
-    ALTER_ADD_SYSTEM_VERSIONING = 1L << 30,
-    ALTER_DROP_SYSTEM_VERSIONING= 1L << 31,
-  };
 
   enum enum_enable_or_disable { LEAVE_AS_IS, ENABLE, DISABLE };
 
@@ -176,7 +105,7 @@ public:
   };
   List<Virtual_column_info>     check_constraint_list;
   // Type of ALTER TABLE operation.
-  uint                          flags;
+  alter_table_operations        flags;
   // Enable or disable keys.
   enum_enable_or_disable        keys_onoff;
   // List of partitions.
